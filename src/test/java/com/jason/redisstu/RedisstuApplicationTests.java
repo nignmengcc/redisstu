bb8757f9605f6fe4aa394bd160c80f0132cf927f
nignmengcc <1208931394@qq.com>
2022-01-09 00:29:22 +0800

    dd

---
+++ src/test/java/com/jason/redisstu/RedisstuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.redisstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisstuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
